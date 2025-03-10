e4ce54010f33d63b5fa51ab07daf59d8372a8346
jeninchi <jenett.cheng@gmail.com>
2025-03-10 12:17:29 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,17 +1,24 @@
--- Final Exam: SQL-- Our analysis is based on two datasets: Taxi and Weather. The Taxi dataset was initially obtained via an API, processed in Python, and underwent exploratory data analysis (EDA) and cleaning before being imported into Snowflake. A key transformation applied was rounding timestamps to the nearest hour, which enables accurate joins with the weather data. The Weather dataset was sourced from Open-Meteo, providing historical weather conditions such as temperature and precipitation. This structured approach allows us to analyze how weather impacts taxi rides, fares, and tipping behavior.-- Inspection
+-- Final Exam: SQL-- Our analysis is based on two datasets: Taxi and Weather. The Taxi dataset was initially obtained via an API, processed in Python, and underwent exploratory data analysis (EDA) and cleaning before being imported into Snowflake. A key transformation applied was rounding timestamps to the nearest hour, which enables accurate joins with the weather data. The Weather dataset was sourced from Open-Meteo, providing historical weather conditions such as temperature and precipitation. This structured approach allows us to analyze how weather impacts taxi rides, fares, and tipping behavior.
+-- Inspection
 -- Here, we are looking at the first 10 rows of each table. 
 SELECT * FROM TAXI.PUBLIC.TAXI LIMIT 10; 
 SELECT * FROM TAXI.PUBLIC.WEATHER LIMIT 10; -- Query 1: Counting total rides per hour by date
 SELECT ROUNDED_DATETIME AS hour, COUNT(*) AS total_rides
 FROM TAXI.PUBLIC.TAXI
 GROUP BY 1 -- 1 refers to the first column in the SELECT statement. In this case, ROUNDED_DATETIME
-ORDER BY 1;-- This query gives an overview of ride volume per hour, helping us identify daily demand trends. -- Query 2: Join 1 - Weather variables and total rides by hour by date
+ORDER BY 1;
+-- This query gives an overview of ride volume per hour, helping us identify daily demand trends. 
+
+-- Query 2: Join 1 - Weather variables and total rides by hour by date
 SELECT t.ROUNDED_DATETIME, w.TEMPERATURE_2MF, w.PRECIPITATION, w.RAIN, COUNT(*) AS total_rides
 FROM TAXI.PUBLIC.TAXI as t
 LEFT JOIN TAXI.PUBLIC.WEATHER as w
 ON t.ROUNDED_DATETIME = w.TIME
 GROUP BY t.ROUNDED_DATETIME, w.TEMPERATURE_2MF, w.PRECIPITATION, w.RAIN
-ORDER BY 1;-- This join merges weather data with taxi ride counts, allowing us to see how weather conditions impact ride volume. -- Query 3: Join 2 - What is the average fare & distance per hour when there's precipitation vs. no precipitation by hour of the day?
+ORDER BY 1;
+-- This join merges weather data with taxi ride counts, allowing us to see how weather conditions impact ride volume. 
+
+-- Query 3: Join 2 - What is the average fare & distance per hour when there's precipitation vs. no precipitation by hour of the day?
 SELECT 
     EXTRACT(HOUR FROM t.ROUNDED_DATETIME) AS hour_of_day,
     CASE 
@@ -24,8 +31,11 @@ FROM TAXI.PUBLIC.TAXI t
 JOIN TAXI.PUBLIC.WEATHER w 
 ON t.ROUNDED_DATETIME = w.TIME
 GROUP BY 1, 2
-ORDER BY hour_of_day, rain_status DESC;-- This query examines how precipitation affects trip fares and distances, helping to assess how weather influences taxi usage.
--- We notice from the output that not all hours include information with precipitation/no precipitation. In addition, we see no clear trend on whether precipitation affects average fare or distance.-- Query 4: Join 3 - Tip percentage by ride length and weather condition
+ORDER BY hour_of_day, rain_status DESC;
+-- This query examines how precipitation affects trip fares and distances, helping to assess how weather influences taxi usage.
+-- We notice from the output that not all hours include information with precipitation/no precipitation. In addition, we see no clear trend on whether precipitation affects average fare or distance.
+
+-- Query 4: Join 3 - Tip percentage by ride length and weather condition
 SELECT 
     CASE 
         WHEN w.PRECIPITATION > 0 THEN 'Rainy/Snowy'
@@ -42,14 +52,19 @@ JOIN TAXI.PUBLIC.WEATHER w
 ON t.ROUNDED_DATETIME = w.TIME
 GROUP BY 1, 2
 ORDER BY avg_tip_percentage DESC;-- This analysis reveals how tipping behavior varies based on ride length and weather conditions. 
--- Tip percentage is higher for short rides with rainy/snowy weather. However, most people generally stick with around 9-10% tips. Finally, we see no information for clear weather and short rides; we assume this is because people choose to walk in that scenario. -- Query 5: Window Function 1 - Rank Top Busiest Hours for Taxi Rides
+-- Tip percentage is higher for short rides with rainy/snowy weather. However, most people generally stick with around 9-10% tips. Finally, we see no information for clear weather and short rides; we assume this is because people choose to walk in that scenario. 
+
+-- Query 5: Window Function 1 - Rank Top Busiest Hours for Taxi Rides
 SELECT EXTRACT(HOUR FROM ROUNDED_DATETIME) AS hour_of_day,
        COUNT(*) AS ride_count,
        RANK() OVER (ORDER BY COUNT(*) DESC) AS rank
 FROM TAXI.PUBLIC.TAXI
 GROUP BY 1
-ORDER BY rank;-- This query ranks hours by ride volume, helping to pinpoint peak demand periods.
--- Evening hours are the most popular (4 pm onwards), with 7 pm being the most popular hour. This pattern makes sense since our data is highly skewed around New Year's Eve, when people are out and about. In parallel, morning hours between 2 am and 7 am are significantly less popular. -- Query 6: Window Function 2 - Calculating ride percentage change by hour of the day
+ORDER BY rank;
+-- This query ranks hours by ride volume, helping to pinpoint peak demand periods.
+-- Evening hours are the most popular (4 pm onwards), with 7 pm being the most popular hour. This pattern makes sense since our data is highly skewed around New Year's Eve, when people are out and about. In parallel, morning hours between 2 am and 7 am are significantly less popular. 
+
+-- Query 6: Window Function 2 - Calculating ride percentage change by hour of the day
 SELECT EXTRACT(HOUR FROM ROUNDED_DATETIME) AS hour_of_day,
        COUNT(*) AS ride_count,
        LAG(COUNT(*)) OVER (ORDER BY EXTRACT(HOUR FROM ROUNDED_DATETIME)) AS previous_hour_rides,
@@ -57,14 +72,20 @@ SELECT EXTRACT(HOUR FROM ROUNDED_DATETIME) AS hour_of_day,
        NULLIF(LAG(COUNT(*)) OVER (ORDER BY EXTRACT(HOUR FROM ROUNDED_DATETIME)), 0) * 100 AS ride_percentage_change
 FROM TAXI.PUBLIC.TAXI
 GROUP BY 1
-ORDER BY 1;-- By comparing ride counts hour over hour, this query identifies periods of sudden demand increases or drops.
--- In conjunction with our previous query, we see a 55% increase in ride count between 6 a.m. and 7 a.m. -- Query 7: Advanced Aggregation 1 (Group By) - Calculating average fare for each hour of the day
+ORDER BY 1;
+-- By comparing ride counts hour over hour, this query identifies periods of sudden demand increases or drops.
+-- In conjunction with our previous query, we see a 55% increase in ride count between 6 a.m. and 7 a.m. 
+
+-- Query 7: Advanced Aggregation 1 (Group By) - Calculating average fare for each hour of the day
 SELECT EXTRACT(HOUR FROM ROUNDED_DATETIME) AS hour_of_day, 
        AVG(TOTAL_AMOUNT) AS avg_fare
 FROM TAXI.PUBLIC.TAXI
 GROUP BY 1
-ORDER BY avg_fare DESC;-- This query highlights which hours have the highest and lowest average fares, which is helpful for pricing insights.
--- Average fare is highest for less popular times of the day, such as 6 a.m. -- Query 8: Advanced Aggregation 2 (Cube) - Analyzing the impact of temperature on average fare  
+ORDER BY avg_fare DESC;
+-- This query highlights which hours have the highest and lowest average fares, which is helpful for pricing insights.
+-- Average fare is highest for less popular times of the day, such as 6 a.m. 
+
+-- Query 8: Advanced Aggregation 2 (Cube) - Analyzing the impact of temperature on average fare  
 SELECT 
     EXTRACT(HOUR FROM t.ROUNDED_DATETIME) AS hour_of_day, 
     CASE 
@@ -90,8 +111,11 @@ FROM (
     GROUP BY 1
 )
 ORDER BY avg_fare_per_mile ASC, avg_trip_distance DESC
-LIMIT 10;-- This query helps identify hours with the highest fares per mile, which may indicate premium pricing trends.
--- In addition to being the most expensive average fare, 6 a.m. is also the most expensive fare per mile. For instance, it seems people are not concerned about expensive taxi fares at 6 a.m., and even less so if it's below freezing. -- Query 10: Subquery 2 - Finding the cheapest fare per mile by hour of day
+LIMIT 10;
+-- This query helps identify hours with the highest fares per mile, which may indicate premium pricing trends.
+-- In addition to being the most expensive average fare, 6 a.m. is also the most expensive fare per mile. For instance, it seems people are not concerned about expensive taxi fares at 6 a.m., and even less so if it's below freezing. 
+
+-- Query 10: Subquery 2 - Finding the cheapest fare per mile by hour of day
 SELECT hour_of_day, avg_fare_per_mile, avg_trip_distance, avg_total_amount
 FROM (
     SELECT EXTRACT(HOUR FROM ROUNDED_DATETIME) AS hour_of_day,
@@ -102,5 +126,6 @@ FROM (
     GROUP BY 1
 )
 ORDER BY avg_fare_per_mile DESC
-LIMIT 10;-- This analysis identifies the hours passengers get the best price per mile, offering insight into fare efficiency.
+LIMIT 10;
+-- This analysis identifies the hours passengers get the best price per mile, offering insight into fare efficiency.
 -- We notice that at 7 p.m. fares are the lowest per mile. As we saw previously, 7 p.m. is the most popular time. Demand and supply seem to be better matched for this hour, leading to lower prices.
